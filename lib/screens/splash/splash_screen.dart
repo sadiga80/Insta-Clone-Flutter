ea4caf5620a1ce46ab85d173dcc32e9e23eb9f26
Santhosh Adiga U <sadiga80@gmail.com>
2023-02-25 12:56:58 +0530

    minor change

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -21,7 +21,10 @@ class SplashScreen extends StatelessWidget {
     return WillPopScope(
       onWillPop: () async => false,
       child: BlocListener<AuthBloc, AuthState>(
-        listenWhen: (prevState, state) => prevState.status != state.status, //To prevent the BlocListener from being called multiple times when the AuthState status is the same
+        listenWhen: (prevState, state) =>
+            prevState.status !=
+            state
+                .status, //To prevent the BlocListener from being called multiple times when the AuthState status is the same
         listener: (context, state) {
           if (state.status == AuthStatus.unauthenticated) {
             Navigator.of(context).pushNamed(LoginScreen.routeName);
